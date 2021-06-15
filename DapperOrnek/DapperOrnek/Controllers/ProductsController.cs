5aa2a14252263c0fb04a1d743649b8bf17d6f7fb
Tutku Argun <tutkuargun@outlook.com>
2021-06-15 16:49:04 +0300

    Add files via upload

---
+++ DapperOrnek/DapperOrnek/Controllers/ProductsController.cs
@@ -0,0 +1,61 @@
+﻿using DapperOrnek.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperOrnek.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly ProductRepository productRepository;
+        public ProductsController()
+        {
+            productRepository = new ProductRepository();
+        }
+        //Get All
+        [HttpGet]
+        [Route("Get")]
+        public IList<Product> GetAll()
+        {
+            return productRepository.GetAll();
+        }
+        //Get By Id
+        [HttpGet]
+        [Route("Get/{id}")]
+        public Product GetById(int id)
+        {
+            return productRepository.GetById(id);
+        }
+
+        //Insert
+        [HttpPost]
+        public void Post([FromBody] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                productRepository.AddProduct(product);
+            }
+        }
+        //Update 
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] Product product)
+        {
+            product.ProductId = id;
+            if (ModelState.IsValid)
+            {
+                productRepository.Update(product);
+            }
+        }
+        [HttpDelete]
+        public void Delete(int id, [FromBody] Product product)
+        {
+            product.ProductId = id;
+            productRepository.Delete(product);
+        }
+    }
+}
---
+++ DapperOrnek/DapperOrnek/Models/ProductRepository.cs
@@ -0,0 +1,86 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperOrnek.Models
+{
+    public class ProductRepository
+    {
+        private string conn;
+        public ProductRepository()
+        {
+            conn = "Data Source=(localdb)\\MssqlLocalDb;Initial Catalog=NorthwindDapper;Integrated Security=True";
+        }
+        public IDbConnection Connection
+        {
+            get
+            {
+                return new SqlConnection(conn);
+            }
+        }
+        //GET
+        public IList<Product> GetAll()
+        {
+            List<Product> products;
+            using (var sqlConnection = new SqlConnection(conn))
+            {
+
+                sqlConnection.Open();
+                products = sqlConnection.Query<Product>("SELECT * FROM Products").ToList();
+
+            }
+            return products;
+        }
+        //GET BY ID
+        public Product GetById(int id)
+        {
+            
+            using (var sqlConnection = new SqlConnection(conn))
+            {
+                sqlConnection.Open();
+                string sql = @"SELECT * FROM Products WHERE ProductId=@ProductId";
+                return sqlConnection.Query<Product>(sql, new { ProductId = id }).FirstOrDefault();
+            }
+        }
+        //INSERT
+        public void AddProduct(Product product)
+        {
+            using (var sqlConnection = new SqlConnection(conn))
+            {
+
+                sqlConnection.Open();
+                string sql = @"INSERT INTO Products ( ProductName, UnitPrice, UnitsInStock, Discontinued) VALUES ( @ProductName, @UnitPrice, @UnitsInStock, @Discontinued) ";
+                sqlConnection.Query(sql, product);
+
+            }
+        }
+        //UPDATE:
+        public void Update(Product product)
+        {
+            using (var sqlConnection = new SqlConnection(conn))
+            {
+
+                sqlConnection.Open();
+                string sql = @"UPDATE Products SET ProductName = @ProductName, UnitPrice = @UnitPrice, UnitsInStock  = @UnitsInStock, Discontinued=@Discontinued WHERE ProductId = @ProductId";
+                sqlConnection.Query(sql, product);
+
+            }
+        }
+        //DELETE 
+        public void Delete(Product product)
+        {
+            using (var sqlConnection = new SqlConnection(conn))
+            {
+
+                sqlConnection.Open();
+                string sql = @"DELETE FROM Products WHERE ProductId = @ProductId";
+                sqlConnection.Query(sql, product);
+
+            }
+        }
+    }
+}
